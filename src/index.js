6718df5d4aa5c54edd55ea169d3fe0073d8ec0dc
alibaba_zhangyabin <zhangyabin@git.shhlee.com>
2020-08-17 14:07:06 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import './style.css'
 import Icon from './Icon.png'
 import Data from './data.xml'
 import csvData from './csvData.csv'
+import pringMe from './print.js'
 function component () {
     var element = document.createElement('div')
+    var btn = document.createElement('button')
+    btn.innerHTML = 'Cliek me to check the console!'
+    btn.onclick = pringMe
     element.innerHTML = "Hello, Webpack!";
     element.classList.add('hello')
 
     var img = new Image()
     img.src = Icon
 
+    element.appendChild(btn)
     element.appendChild(img)
 
     console.log(Data)
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function pringMe () {
+    console.log('I get called from pring.js!');
+}
