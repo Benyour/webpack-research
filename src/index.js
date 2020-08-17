cfd510e4435ec09ed1a2a4405dc226ba1e0f2fe1
alibaba_zhangyabin <zhangyabin@git.shhlee.com>
2020-08-17 13:15:10 +0800

    使用file-loader打包图片

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './style.css'
+import Icon from './Icon.png'
 function component () {
     var element = document.createElement('div')
     element.innerHTML = "Hello, Webpack!";
     element.classList.add('hello')
+
+    var img = new Image()
+    img.src = Icon
+
+    element.appendChild(img)
     return element;
 }
 document.body.appendChild(component())
