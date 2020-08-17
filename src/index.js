dadd35a5dc6d3c86fb970ddf02f02ae95b1da455
alibaba_zhangyabin <zhangyabin@git.shhlee.com>
2020-08-17 13:37:33 +0800

    使用csv-loader和xml-loader打包csv\tsv\xml这三种类型的文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css'
 import Icon from './Icon.png'
+import Data from './data.xml'
+import csvData from './csvData.csv'
 function component () {
     var element = document.createElement('div')
     element.innerHTML = "Hello, Webpack!";
@@ -9,6 +11,10 @@ function component () {
     img.src = Icon
 
     element.appendChild(img)
+
+    console.log(Data)
+    console.log(csvData)
+
     return element;
 }
 document.body.appendChild(component())
